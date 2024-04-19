d6093f42fcba3de770872169f78410c277e2779d
muxammad <ibrohimovmuhammad2020@gmail.com>
2024-04-19 16:25:30 +0200

    complete merg_sort

--- merg_sort.c
+++ merg_sort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// prototypes for functions
 void merge_sorted(int a[], int length);
 void merge_recursion(int a[], int l, int r);
 void merge_sorted_list(int a[], int l, int m, int r);
@@ -18,5 +19,55 @@ int main(int argc, char  **argv)
 
 void merge_sorted(int a[], int length)
 {
+    merge_recursion(a, 0, length - 1);
+}
+
+void merge_recursion(int a[], int l, int r)
+{
+    if (l < r) // in order not make a recursion infinite
+    {
+        int mid = l + (r - l ) / 2;
+
+        merge_recursion(a, l, mid);
+        merge_recursion(a, mid + 1, r);
+        merge_sorted_list(a, l, mid, r);
+    }
+}
+
+void merge_sorted_list(int a[], int l, int m, int r)
+{
+    int left_length = m - l + 1; // find a length of the left array
+    int right_length = r - m; // find a length of the right array
+
+    int temp_left[left_length];
+    int temp_right[right_length];
+
+    int i, j, k;
+
+    for (int i = 0; i < left_length; i++)
+        temp_left[i] = a[l + i];
     
+    for (int i = 0; i < right_length; i++)
+        temp_right[i] = a[m + 1 + i];
+
+    for (i = 0, j = 0, k = l; k <= r; k++)
+    {
+        if ((i < left_length) &&
+            ((j >= right_length) || (temp_left[i] <= temp_right[j])))
+        {
+            a[k] = temp_left[i];
+            i++;
+        }
+        else
+        {
+            a[k] = temp_right[j];
+            j++;
+        }
+        
+    }
+    
+
+
+
+
 }
